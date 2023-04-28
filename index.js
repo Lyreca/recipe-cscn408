fd6bd6d97326e96bca35b120bb94457d781192f4
Joann94 <jyang20@liberty.edu>
2023-04-28 14:35:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const port = 5000;
+const port = 3000;
 
 app.get('/', (req, res) => {
     res.sendFile('/index.html', {root: __dirname}); 
