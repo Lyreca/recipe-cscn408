08bc41ede73cd9c048a5c3c7469509869ec9817c
jplohman01 <jplohman@liberty.edu>
2023-04-28 14:52:38 -0400

    checkbox list

--- public/js/displayIngredients.js
+++ public/js/displayIngredients.js
@@ -1,38 +1,28 @@
-var ingredientCategories = [
-    {name: "Fruits", id: "fruits", 
-        ingredients: [
+var ingredients = [
             {name: "Apple", id: "apple"},
             {name: "Banana", id: "banana"},
             {name: "Orange", id: "orange"},
             {name: "Pear", id: "pear"},
-            {name: "Strawberry", id: "strawberry"}
-        ]  
-    },
-    {name: "Vegetables", id: "vegetables",
-        ingredients: [
+            {name: "Strawberry", id: "strawberry"},
             {name: "Lettuce", id: "lettuce"},
             {name: "Tomato", id: "tomato"},
             {name: "Carrot", id: "carrot"},
             {name: "Cucumber", id: "cucumber"},
-            {name: "Onion", id: "onion"}
-        ]
-    },
-    {name: "Meat", id: "meat",
-        ingredients: [
+            {name: "Onion", id: "onion"},
             {name: "Chicken", id: "chicken"},
             {name: "Beef", id: "beef"},
             {name: "Pork", id: "pork"},
             {name: "Lamb", id: "lamb"},
             {name: "Turkey", id: "turkey"}
-        ]
-    }
-];
+        ];
 
 function displayIngredients() {
-    document.getElementById("ingredientCategories").innerHTML = 
-        ingredientCategories.map(category => 
-            `<div class="ingredientCategory">
-                <p>${category.name}</p>
+    document.getElementById("ingredients").innerHTML = 
+        ingredients.map(ingredient => 
+            `<div class="ingredients">
+                <label>
+                    <input type="checkbox" name=${ingredient.name} value=${ingredient.id}>${ingredient.name}</input>
+                </label>
             </div>`
         ).join("");
 }
