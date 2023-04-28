a32836a35aec058dc9b563e60e434c80a6b01127
Frans Santoso <fsantoso@liberty.edu>
2023-04-28 16:28:14 -0400

    implemented basic checkbox functionality using div

--- public/js/displayIngredients.js
+++ public/js/displayIngredients.js
@@ -17,12 +17,32 @@ var ingredients = [
         ];
 
 function displayIngredients() {
-    document.getElementById("ingredients").innerHTML = 
-        ingredients.map(ingredient => 
-            `<div class="ingredients">
-                <label>
-                    <input type="checkbox" name=${ingredient.name} value=${ingredient.id}>${ingredient.name}</input>
-                </label>
-            </div>`
-        ).join("");
+    ingredients.map(ingredient => {
+            var ingredientDiv = document.createElement("div");
+            ingredientDiv.classList.add("selection");
+            ingredientDiv.id = ingredient.id;
+            ingredientDiv.innerHTML = ingredient.name;
+            document.getElementById("ingredientsUnselected").appendChild(ingredientDiv);
+        }
+    )
+}
+
+function highlightIngredients() {
+    const selections = document.querySelectorAll(".selection");
+    const selected = document.querySelectorAll(".selected");
+
+    selections.forEach(selection => {
+        selection.addEventListener("click", () => {
+                if (selection.classList.contains("selected")) {
+                    selection.classList.remove("selected");
+                    document.getElementById("ingredientsSelected").removeChild(selection);
+                    document.getElementById("ingredientsUnselected").appendChild(selection);
+                } else {
+                    selection.classList.add("selected");
+                    document.getElementById("ingredientsUnselected").removeChild(selection);
+                    document.getElementById("ingredientsSelected").appendChild(selection);
+                }
+            })
+        }
+    );
 }
