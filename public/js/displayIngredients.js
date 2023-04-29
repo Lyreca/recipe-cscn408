e8fccc1e3fd7fea5afe1f72e8ccb768968106c7d
Frans Santoso <fsantoso@liberty.edu>
2023-04-28 22:28:15 -0400

    implemented API ingredient list

--- public/js/displayIngredients.js
+++ public/js/displayIngredients.js
@@ -1,23 +1,34 @@
-var ingredients = [
-            {name: "Apple", id: "apple"},
-            {name: "Banana", id: "banana"},
-            {name: "Orange", id: "orange"},
-            {name: "Pear", id: "pear"},
-            {name: "Strawberry", id: "strawberry"},
-            {name: "Lettuce", id: "lettuce"},
-            {name: "Tomato", id: "tomato"},
-            {name: "Carrot", id: "carrot"},
-            {name: "Cucumber", id: "cucumber"},
-            {name: "Onion", id: "onion"},
-            {name: "Chicken", id: "chicken"},
-            {name: "Beef", id: "beef"},
-            {name: "Pork", id: "pork"},
-            {name: "Lamb", id: "lamb"},
-            {name: "Turkey", id: "turkey"}
-        ];
+// var ingredients = [
+//             {name: "Apple", id: "apple"},
+//             {name: "Banana", id: "banana"},
+//             {name: "Orange", id: "orange"},
+//             {name: "Pear", id: "pear"},
+//             {name: "Strawberry", id: "strawberry"},
+//             {name: "Lettuce", id: "lettuce"},
+//             {name: "Tomato", id: "tomato"},
+//             {name: "Carrot", id: "carrot"},
+//             {name: "Cucumber", id: "cucumber"},
+//             {name: "Onion", id: "onion"},
+//             {name: "Chicken", id: "chicken"},
+//             {name: "Beef", id: "beef"},
+//             {name: "Pork", id: "pork"},
+//             {name: "Lamb", id: "lamb"},
+//             {name: "Turkey", id: "turkey"}
+//         ];
+
+var ingredientList = []
+
+function readCSVData(data) {
+    const rows = data.split("\n");
+    rows.slice(1, -1).forEach(row => {
+        row = row.slice(0,-1);
+        const columns = row.split(";");
+        ingredientList.push({name: columns[0], id: columns[1]});
+    });
+}
 
 function displayIngredients() {
-    ingredients.map(ingredient => {
+    ingredientList.map(ingredient => {
             var ingredientDiv = document.createElement("div");
             ingredientDiv.classList.add("selection");
             ingredientDiv.id = ingredient.id;
