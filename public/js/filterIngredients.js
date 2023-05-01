013eae6c62d746079c471eb73d7109f488b466f9
Frans Santoso <fsantoso@liberty.edu>
2023-05-01 12:03:03 -0400

    made filter work after unselecting

--- public/js/filterIngredients.js
+++ public/js/filterIngredients.js
@@ -1,27 +1,29 @@
 function filterIngredients() {
     var input = document.getElementById("searchBarIngredients");
+    input.addEventListener("keyup", filter);
+}
 
-    input.addEventListener("keyup", function(event) {
-        var ingredients = document.getElementById("ingredientsUnselected");
-        var selection = ingredients.getElementsByClassName("selection");
+function filter() {
+    var input = document.getElementById("searchBarIngredients");
+    var ingredients = document.getElementById("ingredientsUnselected");
+    var selection = ingredients.getElementsByClassName("selection");
 
-        if(input.value !== "") {
-            var filter = input.value.toUpperCase();
+    if(input.value !== "") {
+        var filter = input.value.toUpperCase();
 
-            for (var i = 0; i < selection.length; i++) {
-                var txtValue = selection[i].textContent || selection[i].innerText;
-                if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                    selection[i].style.display = "";
-                } else {
-                    selection[i].style.display = "none";
-                }
-            }
-        } 
-        else {
-            // reset
-            for (var i = 0; i < selection.length; i++) {
+        for (var i = 0; i < selection.length; i++) {
+            var txtValue = selection[i].textContent;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
                 selection[i].style.display = "";
+            } else {
+                selection[i].style.display = "none";
             }
         }
-    });
+    } 
+    else {
+        // reset
+        for (var i = 0; i < selection.length; i++) {
+            selection[i].style.display = "";
+        }
+    }
 }
